524fa2e0342d9522e631ae97556600bed4e2876b
LangChuan Huang <er230059@gmail.com>
2016-03-17 18:54:46 +0800

    Update Vsnr.js - plus 1 or sub 1 when every receiver date

--- static/js/Vsnr.js
+++ static/js/Vsnr.js
@@ -4,23 +4,25 @@ var ctRST = true;
 var rstTimer = setTimeout(rstCtRST, 15000);
 
 var nData = []; //index 0 = t-2, 1 = t-1, 2 = t(latest)
-var vsnr = {}, nMax = {}, nMin = {};
+var vsnr = {};
+var plusData = true;
 
 function rstCtRST() {
 	ctRST = false;
-	for(var i in vsnr) {
-		for(var j = 0; j < vsnr[i].length; j++){
-			if((Math.abs(nMax[i][j] - nMin[i][j]) < minVppLimit) || vsnr[i][j] == 0 || parseInt(nData[2][i]['status'][j]) == 1 && parseInt(nData[1][i]['status'][j]) == 1 && parseInt(nData[0][i]['status'][j]) == 1) {
-				vsnr[i][j] = Infinity;
-			}
-		}
-	}
 }
 
 var socket = io();
 
 socket.on('update', function (data) {
 	var json = JSON.parse(data);
+	
+	for(var i = 0; i < data.length; i++) {
+		for(var j = 0; j < data[i]['data'].length; j++) {
+			data[i]['data'][j] += (plusData ? 1 : -1);			
+		}
+	}
+	plusData = !plusData;
+	
 	nData.push(json);
 	if(nData.length > 3) {
 		nData.shift();
@@ -32,18 +34,7 @@ socket.on('update', function (data) {
 				for(var k = 0; k < 3; k++) {
 					nValue.push(parseInt(nData[k][i]['data'][j]));
 				}
-				nMax[nData[0][i]['id']] = nMax[nData[0][i]['id']] || [];
-				nMax[nData[0][i]['id']][j] = nMax[nData[0][i]['id']][j] || 0;
-				nMin[nData[0][i]['id']] = nMin[nData[0][i]['id']] || [];
-				nMin[nData[0][i]['id']][j] = nMin[nData[0][i]['id']][j] || Infinity;
-
-				if(nValue[2] > nMax[nData[0][i]['id']][j]) {
-					nMax[nData[0][i]['id']][j] = nValue[2];
-				}
-				if(nValue[2] < nMin[nData[0][i]['id']][j]) {
-					nMin[nData[0][i]['id']][j] = nValue[2];
-				}
-
+						
 				vsnr[nData[0][i]['id']] = vsnr[nData[0][i]['id']] || [];
 				if((nValue[1] - nValue[0] < 0) && (nValue[2] - nValue[1] > 0)) {
 					vsnr[nData[0][i]['id']][j] = vsnr[nData[0][i]['id']][j] + 1 || 1; //first dimensional = sensor_id, second = index(channel) in sensor_id
@@ -71,8 +62,6 @@ socket.on('update', function (data) {
 				rstTimer = undefined;
 			}
 			vsnr = {};
-			nMax = {};
-			nMin = {};
 			ctRST = true;
 			rstTimer = setTimeout(rstCtRST, 15000);
 		}
